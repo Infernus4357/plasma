ff34c010b6ac999e13e2295ea707bdfdc05cc3fe
Gautam <infernus4357@gmail.com>
2024-04-15 14:11:12 +0530

    Create enums.py

---
+++ core/enums.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+
+class EmbedStyle(Enum):
+    SUCCESS = {"color": 0x2ECC71, "emoji": "<:_:1223561046954872872>"}
+    FAILURE = {"color": 0xE74C3C, "emoji": "<:_:1223558879795744768>"}

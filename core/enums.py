584221bc8b7942c038af7c7433006f845d8db44f
Gautam <infernus4357@gmail.com>
2024-04-17 16:55:47 +0530

    Update enums.py

--- core/enums.py
+++ core/enums.py
@@ -2,5 +2,5 @@ from enum import Enum
 
 
 class EmbedStyle(Enum):
-    SUCCESS = {"color": 0x2ECC71, "emoji": "<:_:1223561046954872872>"}
-    FAILURE = {"color": 0xE74C3C, "emoji": "<:_:1223558879795744768>"}
+    SUCCESS = {"color": 0x2ECC71, "emoji": "<:_:1230098071291953175>"}
+    FAILURE = {"color": 0xE74C3C, "emoji": "<:_:1230095980506906694>"}
